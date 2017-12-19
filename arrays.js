c0ab266b2364cb40f20e2bbf25fa8fe045f0a621
Felice Forby <feliceforby@gmail.com>
2017-12-19 23:36:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,22 +1,22 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array,element) {
-  [element, ...array]
+  [element, ...array];
   return array
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  array.unshift(element)
+  array.unshift(element);
   return array
 }
 
 function addElementToEndOfArray(array,element) {
-  [...array, element]
+  [...array, element];
   return array
 }
 
 function destructivelyAddElementToEndOfArray(array,element) {
-  array.push(element)
+  array.push(element);
   return array
 }
 
